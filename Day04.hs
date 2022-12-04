eeda53006e6d830e5ce797799eefdd268b176716
Aleksei Pirogov <astynax@users.noreply.github.com>
2022-12-04 09:18:42 +0400

    add a solution for the day #4

---
+++ Day04.hs
@@ -0,0 +1,48 @@
+{-# OPTIONS -Wall #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Day04 where
+
+import Data.Char
+import Data.Set qualified as Set
+import Text.ParserCombinators.ReadP -- Why not?
+
+type Pair = ((Int, Int), (Int, Int))
+
+main :: IO ()
+main = do
+  xs <- map decode . lines <$> readFile "Day04.input"
+  print $ solution1 xs -- 599
+  print $ solution2 xs -- 928
+
+solution1 :: [Pair] -> Int
+solution1 = length . filter areFullyContain . map toSets
+
+solution2 :: [Pair] -> Int
+solution2 = length . filter areOverlap . map toSets
+
+areFullyContain :: (Set.Set Int, Set.Set Int) -> Bool
+areFullyContain (s1, s2) = s1 `Set.isSubsetOf` s2 || s2 `Set.isSubsetOf` s1
+
+areOverlap :: (Set.Set Int, Set.Set Int) -> Bool
+areOverlap (s1, s2) = not . Set.null $ s1 `Set.intersection` s2
+
+toSets :: Pair -> (Set.Set Int, Set.Set Int)
+toSets ((f1, t1), (f2, t2)) = (s1, s2)
+  where
+    s1 = Set.fromList [f1 .. t1]
+    s2 = Set.fromList [f2 .. t2]
+
+decode :: String -> Pair
+decode s = case readP_to_S lineP s of
+  [(x, "")] -> x
+  x -> error $ "Can't parse " <> show x
+
+lineP :: ReadP Pair
+lineP = (,) <$> rangeP <* char ',' <*> rangeP
+
+rangeP :: ReadP (Int, Int)
+rangeP = (,) <$> numP <* char '-' <*> numP
+
+numP :: ReadP Int
+numP = read <$> munch1 isDigit
